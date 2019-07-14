e6e924acf7f9fac976ed6f90deeb5714ce122b04
Emilia Zawisza <emilia.zawisza@wp.pl>
2019-07-14 14:24:37 +0200

    połączenie 2 metod w 1

--- src/main/java/pl/sda/WeatherService.java
+++ src/main/java/pl/sda/WeatherService.java
@@ -5,6 +5,7 @@ import org.apache.commons.io.IOUtils;
 import org.json.JSONObject;
 import pl.sda.model.Current;
 import pl.sda.model.Location;
+import pl.sda.model.Weather;
 
 import java.io.IOException;
 import java.lang.String;
@@ -85,6 +86,38 @@ public class WeatherService {
                 .name(name).build();
 
         return location;
+    }
+
+    public Weather getWeather(){
+        JSONObject jsonObject = new JSONObject(this.data);
+        String temp = jsonObject.getJSONObject("current").get("temp_c").toString();
+        String lat = jsonObject.getJSONObject("location").get("lat").toString();
+        String lon= jsonObject.getJSONObject("location").get("lon").toString();
+        String country= jsonObject.getJSONObject("location").get("country").toString();
+        String name = jsonObject.getJSONObject("location").get("name").toString();
+        String wind_dir = jsonObject.getJSONObject("current").get("wind_dir").toString();
+        String precip_in = jsonObject.getJSONObject("current").get("precip_in").toString();
+
+        Weather weather = new Weather();
+
+        Current current = Current.builder()
+                .temp_c(Double.parseDouble(temp))
+                .wind_dir(wind_dir)
+                .precip_in(Double.parseDouble(precip_in))
+                .build();
+
+        Location location = Location.builder()
+                .lat(Double.parseDouble(lat))
+                .lon(Double.parseDouble(lon))
+                .country(country)
+                .name(name)
+                .build();
+
+        weather.setCurrent(current);
+        weather.setLocation(location);
+
+        return weather;
+
     }
 }
 
